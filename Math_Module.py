359fcb5abd06f9826009b88153b3cfa46fe6c6fd
Paneer Selvam <99405385+paneer24@users.noreply.github.com>
2025-03-31 14:56:56 +0530

    Create Math_Module.py

---
+++ Math_Module.py
@@ -0,0 +1,6 @@
+import math
+n=int(input("Enter a number: "))
+print("Square root of a number is", math.sqrt(n))
+print("Natural logarithm (log base e) of the number is", math.log(n))
+radians=math.radians(n)
+print("Sine of the number (in radians) is", math.sin(radians))
